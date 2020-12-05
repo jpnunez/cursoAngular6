1615a12d88cf7e07ea4ba4205e57d2c30b977163
jpnunez <tojpnunez@gmail.com>
2020-12-05 17:28:25 -0300

    rutas

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -3,20 +3,31 @@
 
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DestinoVIajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+
+const routes: Routes =[
+  { path: '', redirectTo: 'home', pathMatch:'full' },
+  { path: 'home', component: ListaDestinosComponent},  
+  { path: 'destino', component: DestinoDetalleComponent},
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DestinoVIajeComponent,
-    ListaDestinosComponent
+    ListaDestinosComponent,
+    DestinoDetalleComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
